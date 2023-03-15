9ed959a69bde2faf8e2a7bb40b314f6eb4ac2c89
Laravel Daily <info@laraveldaily.com>
2023-03-15 23:52:19 +0000

    QuickAdminPanel automatic commit

---
+++ app/Models/User.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Models;
+
+use App\Support\HasAdvancedFilter;
+use Carbon\Carbon;
+use DateTimeInterface;
+use Hash;
+use Illuminate\Contracts\Translation\HasLocalePreference;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable implements HasLocalePreference
+{
+    use HasFactory, HasAdvancedFilter, Notifiable, SoftDeletes;
+
+    public $table = 'users';
+
+    protected $hidden = [
+        'remember_token',
+        'password',
+    ];
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'locale',
+    ];
+
+    public $orderable = [
+        'id',
+        'name',
+        'email',
+        'email_verified_at',
+        'locale',
+    ];
+
+    protected $dates = [
+        'email_verified_at',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public $filterable = [
+        'id',
+        'name',
+        'email',
+        'email_verified_at',
+        'roles.title',
+        'locale',
+    ];
+
+    public function getIsAdminAttribute()
+    {
+        return $this->roles()->where('title', 'Admin')->exists();
+    }
+
+    public function scopeAdmins()
+    {
+        return $this->whereHas('roles', fn ($q) => $q->where('title', 'Admin'));
+    }
+
+    public function preferredLocale()
+    {
+        return $this->locale;
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function getEmailVerifiedAtAttribute($value)
+    {
+        return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('project.datetime_format')) : null;
+    }
+
+    public function setEmailVerifiedAtAttribute($value)
+    {
+        $this->attributes['email_verified_at'] = $value ? Carbon::createFromFormat(config('project.datetime_format'), $value)->format('Y-m-d H:i:s') : null;
+    }
+
+    public function setPasswordAttribute($input)
+    {
+        if ($input) {
+            $this->attributes['password'] = Hash::needsRehash($input) ? Hash::make($input) : $input;
+        }
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('project.datetime_format')) : null;
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('project.datetime_format')) : null;
+    }
+
+    public function getDeletedAtAttribute($value)
+    {
+        return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('project.datetime_format')) : null;
+    }
+}
